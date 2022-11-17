7bf5d44116fb523cca0dab500b6c25769b23dddc
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2022-11-17 14:19:59 +0100

    Update udata_hydra/utils/http.py
    Co-authored-by: maudetes <maudet.estelle@gmail.com>

--- udata_hydra/utils/http.py
+++ udata_hydra/utils/http.py
@@ -10,7 +10,7 @@ async def send(dataset_id: str, resource_id: str, document: dict) -> None:
     if not config.UDATA_URI or not config.UDATA_URI_API_KEY:
         log.error(g"Missing Udata URI and API key to send http query")
         return
-    uri = f"{config.UDATA_URI}{dataset_id}/resources/{resource_id}/extras/"
+    uri = f"{config.UDATA_URI}/datasets/{dataset_id}/resources/{resource_id}/extras/"
     headers = {"content-type": "application/json", "X-API-KEY": config.UDATA_URI_API_KEY}
     async with aiohttp.ClientSession() as session:
         async with session.put(uri, json=document, headers=headers) as resp:
