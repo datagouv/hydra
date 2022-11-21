5ecaba7dbba7ad3cd7661a2e8a352aa8ef1d848b
maudetes <maudet.estelle@gmail.com>
2022-11-21 18:29:27 +0100

    Allow none in resource document validation and remove None extras

--- udata_hydra/utils/http.py
+++ udata_hydra/utils/http.py
@@ -12,6 +12,10 @@ async def send(dataset_id: str, resource_id: str, document: dict) -> None:
         return
     uri = f"{config.UDATA_URI}/datasets/{dataset_id}/resources/{resource_id}/extras/"
     headers = {"content-type": "application/json", "X-API-KEY": config.UDATA_URI_API_KEY}
+
+    # Extras in udata can't be None
+    document = {key: document[key] for key in document if document[key] is not None}
+
     async with aiohttp.ClientSession() as session:
         async with session.put(uri, json=document, headers=headers) as resp:
             body = await resp.text()
