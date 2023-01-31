d924d50369ef795e0151555c9586546dfbcdf8c9
Alexandre Bulté <alexandre@bulte.net>
2023-01-31 09:41:17 +0100

    Update udata_hydra/utils/csv.py
    Co-authored-by: maudetes <maudet.estelle@gmail.com>

--- udata_hydra/utils/csv.py
+++ udata_hydra/utils/csv.py
@@ -200,11 +200,11 @@ async def perform_csv_inspection(file_path):
 async def detect_csv_from_headers(check) -> bool:
     """Determine if content-type header looks like a csv's one"""
     headers = json.loads(check["headers"] or {})
-    return any([
+    return any(
         headers.get("content-type", "").lower().startswith(ct) for ct in [
             "application/csv", "text/plain", "text/csv"
         ]
-    ])
+    )
 
 
 async def delete_table(table_name: str):
