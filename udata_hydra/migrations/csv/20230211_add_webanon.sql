cb4f3380fde924b68401bcb27cee615cfb5f5b83
Alexandre Bulté <alexandre@bulte.net>
2023-02-11 12:02:44 +0100

    Fix migration for tests

--- udata_hydra/migrations/csv/20230211_add_webanon.sql
+++ udata_hydra/migrations/csv/20230211_add_webanon.sql
@@ -1,6 +1,13 @@
--- add a webanon role for postgrest integration
+-- add a webanon role for postgrest integration (not idempotent)
 -- TODO: we might want a dedicated schema for CSVs
 
-CREATE ROLE web_anon nologin;
-GRANT usage ON SCHEMA public TO web_anon;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO web_anon;
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'web_anon') THEN
+        CREATE ROLE web_anon nologin;
+        GRANT usage ON SCHEMA public TO web_anon;
+        GRANT SELECT ON ALL TABLES IN SCHEMA public TO web_anon;
+   END IF;
+END
+$do$;
