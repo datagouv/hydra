0e2dab0519316e00b177bb9833097e96e6a9b6fb
Pierlou <pierlou.ramade@data.gouv.fr>
2023-11-17 17:56:12 +0100

    feat: add tests for csv.gz processing

---
+++ tests/test_csvgz_analysis.py
@@ -0,0 +1,25 @@
+import hashlib
+import json
+import pytest
+
+from udata_hydra.analysis.csv import analyse_csv
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_csvgz_analysis(rmock, db, fake_check):
+    check = await fake_check()
+    filename, expected_count = ("donnee-dep-data.gouv-2022-geographie2023-produit-le2023-07-17.csv.gz", 9898)
+    url = check["url"]
+    table_name = hashlib.md5(url.encode("utf-8")).hexdigest()
+    with open(f"tests/data/{filename}", "rb") as f:
+        data = f.read()
+    rmock.get(url, status=200, body=data)
+    await analyse_csv(check_id=check["id"])
+    count = await db.fetchrow(f'SELECT count(*) AS count FROM "{table_name}"')
+    assert count["count"] == expected_count
+    profile = await db.fetchrow("SELECT csv_detective FROM tables_index WHERE resource_id = $1", check["resource_id"])
+    profile = json.loads(profile["csv_detective"])
+    for attr in ("header", "columns", "formats", "profile"):
+        assert profile[attr]
+    assert profile["total_lines"] == expected_count
