8e7bcec2c02504b1efca8bfa3729f32fe9edb15e
Pierlou <pierlou.ramade@data.gouv.fr>
2023-11-21 09:37:59 +0100

    fix: crawler test now passses

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -39,8 +39,8 @@ nest_asyncio.apply()
 async def mock_download_resource(url, headers, is_csv):
     tmp_file = tempfile.NamedTemporaryFile(delete=False)
     tmp_file.write(SIMPLE_CSV_CONTENT.encode("utf-8"))
-    # tmp_file.close()
-    return tmp_file
+    tmp_file.close()
+    return tmp_file, True
 
 
 async def test_catalog(setup_catalog, db):
