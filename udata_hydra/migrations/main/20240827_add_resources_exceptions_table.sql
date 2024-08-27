0190b513a6bf6e81add5c0d470bdcd0cfac08978
Adrien Carpentier <me@adriencarpentier.com>
2024-08-27 18:21:06 +0200

    feat: update migration to create resources_exceptions table

--- udata_hydra/migrations/main/20240827_add_resources_exceptions_table.sql
+++ udata_hydra/migrations/main/20240827_add_resources_exceptions_table.sql
@@ -1,9 +1,12 @@
 -- add resources_exceptions table
 
--- create a table with a resource_id column as foreign key to resources.id
+-- create a table with a unique resource_id column as foreign key to resources.id, and a indexes column to list the the table indexes to be created
 CREATE TABLE resources_exceptions (
     id SERIAL PRIMARY KEY,
-    resource_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (resource_id) REFERENCES catalog(id)
+    resource_id UUID UNIQUE NOT NULL REFERENCES catalog(resource_id) ON DELETE CASCADE,
+    indexes JSONB DEFAULT '[]'::JSONB,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+--- Add rows to the resources_exceptions table
+INSERT INTO resources_exceptions (resource_id) VALUES ('f868cca6-8da1-4369-a78d-47463f19a9a3'), ('4babf5f2-6a9c-45b5-9144-ca5eae6a7a6d');
