aa7240f7dc455a54f970004bfc840fb20e2690b7
Adrien Carpentier <me@adriencarpentier.com>
2024-09-03 14:36:15 +0200

    fix: remove inserting data in resources_exceptions table

--- udata_hydra/migrations/main/20240827_add_resources_exceptions_table.sql
+++ udata_hydra/migrations/main/20240827_add_resources_exceptions_table.sql
@@ -8,5 +8,5 @@ CREATE TABLE resources_exceptions (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
---- Add rows to the resources_exceptions table
-INSERT INTO resources_exceptions (resource_id) VALUES ('f868cca6-8da1-4369-a78d-47463f19a9a3'), ('4babf5f2-6a9c-45b5-9144-ca5eae6a7a6d');
+-- --- Add rows to the resources_exceptions table
+-- INSERT INTO resources_exceptions (resource_id) VALUES ('f868cca6-8da1-4369-a78d-47463f19a9a3'), ('4babf5f2-6a9c-45b5-9144-ca5eae6a7a6d');
