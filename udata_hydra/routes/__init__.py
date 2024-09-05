11224e8c4879d85a83914737834dcaf8f2a73680
Adrien Carpentier <me@adriencarpentier.com>
2024-09-05 11:05:40 +0200

    fix: allow to use routes without trailing slashes

--- udata_hydra/routes/__init__.py
+++ udata_hydra/routes/__init__.py
@@ -13,32 +13,33 @@ from udata_hydra.routes.status import get_crawler_status, get_health, get_stats,
 # routes = web.RouteTableDef()
 routes: list = [
     # Routes for checks
-    web.get("/api/checks/latest", get_latest_check, name="get-latest-check"),
-    web.get("/api/checks/latest/", get_latest_check),
-    web.get("/api/checks/all", get_all_checks),
+    web.get("/api/checks/latest/", get_latest_check, name="get-latest-check"),
+    web.get("/api/checks/latest", get_latest_check),
     web.get("/api/checks/all/", get_all_checks),
-    web.post("/api/checks", create_check),
+    web.get("/api/checks/all", get_all_checks),
     web.post("/api/checks/", create_check),
+    web.post("/api/checks", create_check),
     # Routes for resources
-    web.get("/api/resources", get_resource),
     web.get("/api/resources/", get_resource),
-    # web.get("/api/resources/{resource_id}/status", get_resource_status),
+    web.get("/api/resources", get_resource),
     web.get("/api/resources/{resource_id}/status/", get_resource_status),
-    web.post("/api/resources", create_resource),
+    web.get("/api/resources/{resource_id}/status", get_resource_status),
     web.post("/api/resources/", create_resource),
-    web.put("/api/resources", update_resource),
+    web.post("/api/resources", create_resource),
     web.put("/api/resources/", update_resource),
+    web.put("/api/resources", update_resource),
+    web.delete("/api/resources/", delete_resource),
     web.delete("/api/resources", delete_resource),
     web.post("/api/resource/created/", create_resource),  # TODO: legacy, to remove
     web.post("/api/resource/updated/", update_resource),  # TODO: legacy, to remove
     web.post("/api/resource/deleted/", delete_resource),  # TODO: legacy, to remove
     # Routes for statuses
-    web.get("/api/status/crawler", get_crawler_status),
     web.get("/api/status/crawler/", get_crawler_status),
-    web.get("/api/status/worker", get_worker_status),
+    web.get("/api/status/crawler", get_crawler_status),
     web.get("/api/status/worker/", get_worker_status),
-    web.get("/api/stats", get_stats),
+    web.get("/api/status/worker", get_worker_status),
     web.get("/api/stats/", get_stats),
-    web.get("/api/health", get_health),
+    web.get("/api/stats", get_stats),
     web.get("/api/health/", get_health),
+    web.get("/api/health", get_health),
 ]
