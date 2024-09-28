ac6aedd5c77a2b906782afdea3113b6ae1827b85
Adrien Carpentier <adrien.carpentier@mail.numerique.gouv.fr>
2024-09-28 12:38:01 +0900

    fix: fix resources exceptions routes responses, add tests, update README (#176)
    Fix several serious issues with resources exceptions routes:
    
    - Fix serialisation of resource exception in API responses
    - Added a proper Marshmallow schema for resource exception, so that it
    can be used for serialisation
    - Allow posting of a new resources exception with no `tables_indexes`
    - Added end-to-end API tests for all resources exceptions routes cases
    - Remove resources exception routes to be excluded from auth, so that
    POST and DELETE are also authenticated routes
    - Added documentation on how to make a curl request to the API to add a
    ressource exception

--- udata_hydra/schemas/__init__.py
+++ udata_hydra/schemas/__init__.py
@@ -1,3 +1,4 @@
 # ruff: noqa: F401
 from .check import CheckGroupBy, CheckSchema
 from .resource import ResourceDocumentSchema, ResourceSchema
+from .resource_exception import ResourceExceptionSchema
