2f594515c91b40113b6426b619ae77d09a3272a0
Adrien Carpentier <me@adriencarpentier.com>
2024-10-24 12:28:35 +0200

    WIP

--- udata_hydra/crawl/select_batch.py
+++ udata_hydra/crawl/select_batch.py
@@ -1,14 +1,37 @@
+from datetime import datetime, timedelta, timezone
+
 from asyncpg import Record
+from humanfriendly import parse_timespan
 
 from udata_hydra import config, context
 from udata_hydra.db.resource import Resource
 
 
+async def select_rows_based_on_query(connection, q: str, *args) -> list[Record]:
+    """
+    A transaction wrapper around a select query q pass as param with *args.
+    It first creates a temporary table based on this query,
+    then fetches the selected rows and drops the temporary table.
+    It finally returns the selected rows.
+    """
+    temporary_table = "check_urls"
+    create_temp_select_table_query = (
+        f"""CREATE TEMPORARY TABLE {temporary_table} AS {q} FOR UPDATE;"""
+    )
+    async with connection.transaction():
+        await connection.execute("BEGIN;")
+        await connection.execute(create_temp_select_table_query, *args)
+        to_check = await connection.fetch(f"SELECT * FROM {temporary_table};")
+        await connection.execute("COMMIT;")
+    await connection.execute(f"DROP TABLE {temporary_table};")
+    return to_check
+
+
 async def select_batch_resources_to_check() -> list[Record]:
     """Select a batch of resources to check from the catalog
-    1) It first selects resources with priority=True
-    2) ...then resources which have never been checked before
-    3) and if the total number of selected resources is still less than the batch size, it will also add resources with outdated checks in the batch
+    - It first selects resources with priority=True
+    - ...then resources without checks
+    - and if the total number of selected resources is still less than the batch size, it will also add resources with outdated checks in the batch.
     """
     context.monitor().set_status("Getting a batch from catalog...")
 
@@ -16,7 +39,7 @@ async def select_batch_resources_to_check() -> list[Record]:
     async with pool.acquire() as connection:
         excluded = Resource.get_excluded_clause()
 
-        # 1) Resources that are prioritised
+        # first urls that are prioritised
         q = f"""
             SELECT * FROM (
                 SELECT catalog.url, dataset_id, resource_id
@@ -26,9 +49,9 @@ async def select_batch_resources_to_check() -> list[Record]:
             ) s
             ORDER BY random() LIMIT {config.BATCH_SIZE}
         """
-        to_check: list[Record] = await connection.fetch(q)
+        to_check: list[Record] = await select_rows_based_on_query(connection, q)
 
-        # 2) Resources which have never been checked before
+        # then urls without checks
         if len(to_check) < config.BATCH_SIZE:
             q = f"""
                 SELECT * FROM (
@@ -40,66 +63,25 @@ async def select_batch_resources_to_check() -> list[Record]:
                 ) s
                 ORDER BY random() LIMIT {config.BATCH_SIZE}
             """
-            to_check += await connection.fetch(q)
+            to_check += await select_rows_based_on_query(connection, q)
 
-        # 3) if not enough for our batch size, add resources with outdated checks, with an increasing delay if resources are not changing:
-        # To get resources with outdated checks, they need to meet one of the following conditions:
-        # - Don't have at least two checks yet, and the last check is older than CHECK_DELAY_DEFAULT
-        # - At least one the their two most recent last checks have no detected_last_modified_at, and the last check is older than CHECK_DELAY_DEFAULT
-        # - Their two most recent last checks have changed, and the last check is older than CHECK_DELAY_DEFAULT
-        # - Their last two checks have not changed, the two checks have done between two delays in CHECK_DELAYS, and the last check is older than the same delay in CHECK_DELAYS (this is in order to avoid checking too often the same resource which doesn't change)
+        # if not enough for our batch size, handle outdated checks
         if len(to_check) < config.BATCH_SIZE:
+            since = parse_timespan(config.SINCE)  # in seconds
+            since = datetime.now(timezone.utc) - timedelta(seconds=since)
             limit = config.BATCH_SIZE - len(to_check)
-
-            q_start = f"""
-                WITH recent_checks AS (
-                    SELECT resource_id, detected_last_modified_at, created_at,
-                        ROW_NUMBER() OVER (PARTITION BY resource_id ORDER BY created_at DESC) AS rn
-                    FROM checks
-                )
+            q = f"""
+            SELECT * FROM (
                 SELECT catalog.url, dataset_id, catalog.resource_id
-                FROM catalog
-                    LEFT JOIN recent_checks ON catalog.resource_id = recent_checks.resource_id
-                WHERE catalog.priority = False
-                    AND {Resource.get_excluded_clause()}
-                GROUP BY catalog.url, dataset_id, catalog.resource_id
-                HAVING (
-                    (
-                        COUNT(recent_checks.resource_id) < 2
-                        AND MAX(recent_checks.created_at) < NOW() AT TIME ZONE 'UTC' - INTERVAL '{config.CHECK_DELAY_DEFAULT}'
-                    )
-                    OR (
-                        COUNT(CASE WHEN recent_checks.detected_last_modified_at IS NULL THEN 1 END) >= 1
-                        AND MAX(recent_checks.created_at) < NOW() AT TIME ZONE 'UTC' - INTERVAL '{config.CHECK_DELAY_DEFAULT}'
-                    )
-                    OR (
-                        COUNT(DISTINCT recent_checks.detected_last_modified_at) > 1
-                        AND MAX(recent_checks.created_at) < NOW() AT TIME ZONE 'UTC' - INTERVAL '{config.CHECK_DELAYS[0]}'
-                    )
+                FROM catalog, checks
+                WHERE catalog.last_check IS NOT NULL
+                AND {excluded}
+                AND catalog.last_check = checks.id
+                AND checks.created_at <= $1
+                AND catalog.priority = False
+            ) s
+            ORDER BY random() LIMIT {limit}
             """
-
-            q_dynamic = ""
-            for i in range(len(config.CHECK_DELAYS) - 1):
-                q_dynamic += f"""
-                    OR (
-                        COUNT(recent_checks.resource_id) = 2
-                        AND COUNT(DISTINCT recent_checks.detected_last_modified_at) < 2
-                        AND MAX(recent_checks.created_at) < NOW() AT TIME ZONE 'UTC' - INTERVAL '{config.CHECK_DELAYS[i]}'
-                        AND MAX(recent_checks.created_at) >= NOW() AT TIME ZONE 'UTC' - INTERVAL '{config.CHECK_DELAYS[i + 1]}'
-                        AND MIN(recent_checks.created_at) < MAX(recent_checks.created_at) - INTERVAL '{config.CHECK_DELAYS[i]}'
-                        AND MIN(recent_checks.created_at) >= MAX(recent_checks.created_at) - INTERVAL '{config.CHECK_DELAYS[i + 1]}'
-                    )
-                """
-
-            q_end = f"""
-                    OR MAX(recent_checks.created_at) < NOW() AT TIME ZONE 'UTC' - INTERVAL '{config.CHECK_DELAYS[-1]}'
-                )
-                ORDER BY random() LIMIT {limit};
-            """
-
-            # Combine all parts to form the final query
-            q = f"{q_start} {q_dynamic} {q_end}"
-
-            to_check += await connection.fetch(q)
+            to_check += await select_rows_based_on_query(connection, q, since)
 
     return to_check
