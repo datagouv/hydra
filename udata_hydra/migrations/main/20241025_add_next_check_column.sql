b32a703e0da93f120c3b62666d52786ce2fb3c1e
Adrien Carpentier <me@adriencarpentier.com>
2024-11-04 16:06:22 +0100

    fix: fix migration

--- udata_hydra/migrations/main/20241025_add_next_check_column.sql
+++ udata_hydra/migrations/main/20241025_add_next_check_column.sql
@@ -1,4 +1,4 @@
--- Add a new column 'next_check_at' of type DATETIME to the 'checks' table
+-- Add a new column 'next_check_at' of type TIMESTAMPTZ to the 'checks' table
 
 ALTER TABLE checks
-    ADD COLUMN next_check_at DATETIME;
+    ADD COLUMN next_check_at TIMESTAMPTZ;
