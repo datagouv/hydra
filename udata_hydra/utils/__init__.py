19358c17cb363df69d1ef849d90828a3dae43916
Pierlou <pierlou.ramade@data.gouv.fr>
2024-12-18 17:27:29 +0100

    refactor: remove reading and casting utils

--- udata_hydra/utils/__init__.py
+++ udata_hydra/utils/__init__.py
@@ -6,5 +6,4 @@ from .file import compute_checksum_from_file, download_resource, read_csv_gz
 from .geojson import detect_geojson_from_headers_or_catalog
 from .http import UdataPayload, get_request_params, is_valid_uri, send
 from .queue import enqueue
-from .reader import Reader, generate_dialect
 from .timer import Timer
