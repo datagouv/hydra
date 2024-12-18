79e3685ccab6552f8c6409e3f95c9b247d360d4c
Pierlou <pierlou.ramade@data.gouv.fr>
2024-12-18 17:27:29 +0100

    refactor: remove reading and casting utils

--- udata_hydra/analysis/helpers.py
+++ udata_hydra/analysis/helpers.py
@@ -1,36 +1,10 @@
 import json
-from datetime import date, datetime
 from typing import IO
 
 from asyncpg import Record
-from dateparser import parse as date_parser
-from dateutil.parser import ParserError
-from dateutil.parser import parse as dateutil_parser
 
 from udata_hydra import config
 from udata_hydra.utils import UdataPayload, download_resource, queue, send
-
-
-def to_json(value: str) -> str:
-    """Convenience method, should be casted from string directly by postgres"""
-    return value
-
-
-def _parse_dt(value: str) -> datetime | None:
-    """For performance reasons, we try first with dateutil and fallback on dateparser"""
-    try:
-        return dateutil_parser(value)
-    except ParserError:
-        return date_parser(value)
-
-
-def to_date(value: str) -> date | None:
-    parsed = _parse_dt(value)
-    return parsed.date() if parsed else None
-
-
-def to_datetime(value: str) -> datetime | None:
-    return _parse_dt(value)
 
 
 async def read_or_download_file(
