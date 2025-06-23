opendatateam/udata-hydra

refactor: adapt tests

tests/test_analysis/test_analysis_csv.py
@@ -7,13 +7,11 @@ import pytest
 from aiohttp import ClientSession
 from asyncpg.exceptions import UndefinedTableError
 from yarl import URL
+from csv_detective import routine as csv_detective_routine
 
 from tests.conftest import RESOURCE_ID, RESOURCE_URL
 from udata_hydra.analysis.csv import analyse_csv, csv_to_db
-from udata_hydra.crawl.check_resources import (
-    RESOURCE_RESPONSE_STATUSES,
-    check_resource,
-)
+from udata_hydra.crawl.check_resources import check_resource
 from udata_hydra.db.resource import Resource
 
 pytestmark = pytest.mark.asyncio
@@ -93,33 +91,26 @@ async def test_analyse_csv_big_file(setup_catalog, rmock, db, fake_check, produc
     "line_expected",
     (
         # (int, float, string, bool), (__id, int, float, string, bool)
-        ("1,1 020.20,test,true", (1, 1, 1020.2, "test", True), ","),
-        ('2,"1 020,20",test,false', (1, 2, 1020.2, "test", False), ","),
-        ("1;1 020.20;test;true", (1, 1, 1020.2, "test", True), ";"),
-        ("2;1 020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
-        ("2.0;1 020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
+        ("1,1020.20,test,true", (1, 1, 1020.2, "test", True), ","),
+        ('2,"1020,20",test,false', (1, 2, 1020.2, "test", False), ","),
+        ("1;1020.20;test;true", (1, 1, 1020.2, "test", True), ";"),
+        ("2;1020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
+        ("2.0;1020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
     ),
 )
 async def test_csv_to_db_simple_type_casting(db, line_expected, clean_db):
     line, expected, separator = line_expected
+    header = separator.join(['int', 'float', 'string', 'bool'])
     with NamedTemporaryFile() as fp:
-        fp.write(f"int, float, string, bool\n\r{line}".encode("utf-8"))
+        fp.write(f"{header}\n{line}".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            "float": {"python_type": "float"},
-            "string": {"python_type": "string"},
-            "bool": {"python_type": "bool"},
-        }
-        inspection = {
-            "separator": separator,
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            csv_file_path=fp.name,
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = list(await db.fetch("SELECT * FROM test_table"))
     assert len(res) == 1
     cols = ["__id", "int", "float", "string", "bool"]
@@ -148,23 +139,17 @@ async def test_csv_to_db_simple_type_casting(db, line_expected, clean_db):
 async def test_csv_to_db_complex_type_casting(db, line_expected, clean_db):
     line, expected = line_expected
     with NamedTemporaryFile() as fp:
-        fp.write(f"json, date, datetime\n\r{line}".encode("utf-8"))
+        fp.write(f"json;date;datetime\n{line}".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "json": {"python_type": "json"},
-            "date": {"python_type": "date"},
-            "datetime": {"python_type": "datetime"},
-        }
-        inspection = {
-            "separator": ";",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            csv_file_path=fp.name,
+            encoding="utf-8",
+            output_df=True,
+            cast_json=False,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table", debug_insert=True)
     res = list(await db.fetch("SELECT * FROM test_table"))
     assert len(res) == 1
     cols = ["__id", "json", "date", "datetime"]
@@ -176,66 +161,46 @@ async def test_basic_sql_injection(db, clean_db):
     # CREATE TABLE table_name("int" integer, "col_name" text);DROP TABLE toto;--)
     injection = 'col_name" text);DROP TABLE toto;--'
     with NamedTemporaryFile() as fp:
-        fp.write(f"int, {injection}\n\r1,test".encode("utf-8"))
+        fp.write(f"int,{injection}\n1,test".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            injection: {"python_type": "string"},
-        }
-        inspection = {
-            "separator": ",",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            csv_file_path=fp.name,
+            sep=",",
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = await db.fetchrow("SELECT * FROM test_table")
     assert res[injection] == "test"
 
 
 async def test_percentage_column(db, clean_db):
     with NamedTemporaryFile() as fp:
-        fp.write("int, % mon pourcent\n\r1,test".encode("utf-8"))
+        fp.write("int,% mon pourcent\n1,test".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            "% mon pourcent": {"python_type": "string"},
-        }
-        inspection = {
-            "separator": ",",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            csv_file_path=fp.name,
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = await db.fetchrow("SELECT * FROM test_table")
     assert res["% mon pourcent"] == "test"
 
 
 async def test_reserved_column_name(db, clean_db):
     with NamedTemporaryFile() as fp:
-        fp.write("int, xmin\n\r1,test".encode("utf-8"))
+        fp.write("int,xmin\n1,test".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            "xmin": {"python_type": "string"},
-        }
-        inspection = {
-            "separator": ",",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            csv_file_path=fp.name,
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = await db.fetchrow("SELECT * FROM test_table")
     assert res["xmin__hydra_renamed"] == "test"
 

tests/test_parquet_export.py
@@ -1,14 +1,13 @@
 import os
 from io import BytesIO
 
-import pyarrow.parquet as pq
+import pandas as pd
 import pytest
 
 from udata_hydra.analysis.csv import (
     RESERVED_COLS,
     csv_detective_routine,
     csv_to_parquet,
-    generate_records,
 )
 from udata_hydra.utils.parquet import save_as_parquet
 
@@ -26,8 +25,13 @@ pytestmark = pytest.mark.asyncio
 async def test_save_as_parquet(file_and_count):
     filename, expected_count = file_and_count
     file_path = f"tests/data/{filename}"
-    inspection: dict | None = csv_detective_routine(
-        csv_file_path=file_path, output_profile=True, num_rows=-1, save_results=False
+    inspection, df = csv_detective_routine(
+        csv_file_path=file_path,
+        output_profile=True,
+        output_df=True,
+        cast_json=False,
+        num_rows=-1,
+        save_results=False,
     )
     assert inspection
     columns = inspection["columns"]
@@ -35,14 +39,14 @@ async def test_save_as_parquet(file_and_count):
         f"{c}__hydra_renamed" if c.lower() in RESERVED_COLS else c: v["python_type"]
         for c, v in columns.items()
     }
-    _, table = save_as_parquet(
-        records=generate_records(file_path, inspection, columns),
-        columns=columns,
+    _, bytes = save_as_parquet(
+        df=df,
         output_filename=None,
     )
+    table = pd.read_parquet(BytesIO(bytes))
     assert len(table) == expected_count
     fake_file = BytesIO()
-    pq.write_table(table, fake_file)
+    table.to_parquet(fake_file)
 
 
 @pytest.mark.parametrize(
@@ -56,12 +60,17 @@ async def test_save_as_parquet(file_and_count):
 async def test_csv_to_parquet(mocker, parquet_config):
     async def execute_csv_to_parquet() -> tuple[str, int] | None:
         file_path = "tests/data/catalog.csv"
-        inspection: dict | None = csv_detective_routine(
-            csv_file_path=file_path, output_profile=True, num_rows=-1, save_results=False
+        inspection, df = csv_detective_routine(
+            csv_file_path=file_path,
+            output_profile=True,
+            output_df=True,
+            cast_json=False,
+            num_rows=-1,
+            save_results=False,
         )
         assert inspection
         return await csv_to_parquet(
-            file_path=file_path, inspection=inspection, table_name="test_table"
+            df=df, inspection=inspection, table_name="test_table"
         )
 
     csv_to_parquet_config, min_lines_for_parquet_config, expected_conversion = parquet_config

datagouv/hydra

tests/test_analysis/test_analysis_csv.py
@@ -9,6 +9,7 @@ from aiohttp import ClientSession
 from asyncpg.exceptions import UndefinedTableError
 from csv_detective.explore_csv import validate_then_detect
 from yarl import URL
+from csv_detective import routine as csv_detective_routine
 
 from tests.conftest import RESOURCE_ID, RESOURCE_URL
 from udata_hydra.analysis.csv import analyse_csv, csv_to_db
@@ -92,33 +93,26 @@ async def test_analyse_csv_big_file(setup_catalog, rmock, db, fake_check, produc
     "line_expected",
     (
         # (int, float, string, bool), (__id, int, float, string, bool)
-        ("1,1 020.20,test,true", (1, 1, 1020.2, "test", True), ","),
-        ('2,"1 020,20",test,false', (1, 2, 1020.2, "test", False), ","),
-        ("1;1 020.20;test;true", (1, 1, 1020.2, "test", True), ";"),
-        ("2;1 020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
-        ("2.0;1 020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
+        ("1,1020.20,test,true", (1, 1, 1020.2, "test", True), ","),
+        ('2,"1020,20",test,false', (1, 2, 1020.2, "test", False), ","),
+        ("1;1020.20;test;true", (1, 1, 1020.2, "test", True), ";"),
+        ("2;1020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
+        ("2.0;1020,20;test;false", (1, 2, 1020.2, "test", False), ";"),
     ),
 )
 async def test_csv_to_db_simple_type_casting(db, line_expected, clean_db):
     line, expected, separator = line_expected
+    header = separator.join(['int', 'float', 'string', 'bool'])
     with NamedTemporaryFile() as fp:
-        fp.write(f"int, float, string, bool\n\r{line}".encode("utf-8"))
+        fp.write(f"{header}\n{line}".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            "float": {"python_type": "float"},
-            "string": {"python_type": "string"},
-            "bool": {"python_type": "bool"},
-        }
-        inspection = {
-            "separator": separator,
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            file_path=fp.name,
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = list(await db.fetch("SELECT * FROM test_table"))
     assert len(res) == 1
     cols = ["__id", "int", "float", "string", "bool"]
@@ -147,23 +141,17 @@ async def test_csv_to_db_simple_type_casting(db, line_expected, clean_db):
 async def test_csv_to_db_complex_type_casting(db, line_expected, clean_db):
     line, expected = line_expected
     with NamedTemporaryFile() as fp:
-        fp.write(f"json, date, datetime\n\r{line}".encode("utf-8"))
+        fp.write(f"json;date;datetime\n{line}".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "json": {"python_type": "json"},
-            "date": {"python_type": "date"},
-            "datetime": {"python_type": "datetime"},
-        }
-        inspection = {
-            "separator": ";",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            file_path=fp.name,
+            encoding="utf-8",
+            output_df=True,
+            cast_json=False,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table", debug_insert=True)
     res = list(await db.fetch("SELECT * FROM test_table"))
     assert len(res) == 1
     cols = ["__id", "json", "date", "datetime"]
@@ -175,66 +163,46 @@ async def test_basic_sql_injection(db, clean_db):
     # CREATE TABLE table_name("int" integer, "col_name" text);DROP TABLE toto;--)
     injection = 'col_name" text);DROP TABLE toto;--'
     with NamedTemporaryFile() as fp:
-        fp.write(f"int, {injection}\n\r1,test".encode("utf-8"))
+        fp.write(f"int,{injection}\n1,test".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            injection: {"python_type": "string"},
-        }
-        inspection = {
-            "separator": ",",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            file_path=fp.name,
+            sep=",",
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = await db.fetchrow("SELECT * FROM test_table")
     assert res[injection] == "test"
 
 
 async def test_percentage_column(db, clean_db):
     with NamedTemporaryFile() as fp:
-        fp.write("int, % mon pourcent\n\r1,test".encode("utf-8"))
+        fp.write("int,% mon pourcent\n1,test".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            "% mon pourcent": {"python_type": "string"},
-        }
-        inspection = {
-            "separator": ",",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            file_path=fp.name,
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = await db.fetchrow("SELECT * FROM test_table")
     assert res["% mon pourcent"] == "test"
 
 
 async def test_reserved_column_name(db, clean_db):
     with NamedTemporaryFile() as fp:
-        fp.write("int, xmin\n\r1,test".encode("utf-8"))
+        fp.write("int,xmin\n1,test".encode("utf-8"))
         fp.seek(0)
-        columns = {
-            "int": {"python_type": "int"},
-            "xmin": {"python_type": "string"},
-        }
-        inspection = {
-            "separator": ",",
-            "encoding": "utf-8",
-            "header_row_idx": 0,
-            "total_lines": 1,
-            "header": list(columns.keys()),
-            "columns": columns,
-        }
-        # Insert the data
-        await csv_to_db(file_path=fp.name, inspection=inspection, table_name="test_table")
+        inspection, df = csv_detective_routine(
+            file_path=fp.name,
+            output_df=True,
+            num_rows=-1,
+            save_results=False,
+        )
+        await csv_to_db(df=df, inspection=inspection, table_name="test_table")
     res = await db.fetchrow("SELECT * FROM test_table")
     assert res["xmin__hydra_renamed"] == "test"
 

tests/test_analysis/test_parquet_export.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from unittest.mock import MagicMock, patch
 
-import pyarrow.parquet as pq
+import pandas as pd
 import pytest
 
 from tests.conftest import RESOURCE_ID
@@ -9,7 +9,6 @@ from udata_hydra.analysis.csv import (
     RESERVED_COLS,
     csv_detective_routine,
     csv_to_parquet,
-    generate_records,
 )
 from udata_hydra.utils.minio import MinIOClient
 from udata_hydra.utils.parquet import save_as_parquet
@@ -28,8 +27,13 @@ pytestmark = pytest.mark.asyncio
 async def test_save_as_parquet(file_and_count):
     filename, expected_count = file_and_count
     file_path = f"tests/data/{filename}"
-    inspection: dict | None = csv_detective_routine(
-        file_path=file_path, output_profile=True, num_rows=-1, save_results=False
+    inspection, df = csv_detective_routine(
+        file_path=file_path,
+        output_profile=True,
+        output_df=True,
+        cast_json=False,
+        num_rows=-1,
+        save_results=False,
     )
     assert inspection
     columns = inspection["columns"]
@@ -37,14 +41,14 @@ async def test_save_as_parquet(file_and_count):
         f"{c}__hydra_renamed" if c.lower() in RESERVED_COLS else c: v["python_type"]
         for c, v in columns.items()
     }
-    _, table = save_as_parquet(
-        records=generate_records(file_path, inspection, columns),
-        columns=columns,
+    _, bytes = save_as_parquet(
+        df=df,
         output_filename=None,
     )
+    table = pd.read_parquet(BytesIO(bytes))
     assert len(table) == expected_count
     fake_file = BytesIO()
-    pq.write_table(table, fake_file)
+    table.to_parquet(fake_file)
 
 
 @pytest.mark.parametrize(
@@ -58,12 +62,17 @@ async def test_save_as_parquet(file_and_count):
 async def test_csv_to_parquet(mocker, parquet_config):
     async def execute_csv_to_parquet() -> tuple[str, int] | None:
         file_path = "tests/data/catalog.csv"
-        inspection: dict | None = csv_detective_routine(
-            file_path=file_path, output_profile=True, num_rows=-1, save_results=False
+        inspection, df = csv_detective_routine(
+            file_path=file_path,
+            output_profile=True,
+            output_df=True,
+            cast_json=False,
+            num_rows=-1,
+            save_results=False,
         )
         assert inspection
         return await csv_to_parquet(
-            file_path=file_path, inspection=inspection, resource_id=RESOURCE_ID
+            df=df, inspection=inspection, table_name="test_table"
         )
 
     csv_to_parquet_config, min_lines_for_parquet_config, expected_conversion = parquet_config