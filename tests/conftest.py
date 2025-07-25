327063d2de019c758c9305aff971ca27fca5a9f6
Adrien Carpentier <me@adriencarpentier.com>
2025-07-25 17:08:19 +0200

    fix: fix event_loop

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,6 +32,15 @@ pytestmark = pytest.mark.asyncio
 
 nest_asyncio.apply()
 
+
+@pytest.fixture(scope="session")
+def event_loop():
+    """Create an instance of the default event loop for the test session."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
 log = logging.getLogger("udata-hydra")
 
 
