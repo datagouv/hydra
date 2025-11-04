0673526933e1ddf890616764b91523daf4b483f9
Adrien Carpentier <me@adriencarpentier.com>
2025-11-04 14:50:48 +0100

    clean: remove duplicated test added by merge error

--- tests/test_cli/test_purge.py
+++ tests/test_cli/test_purge.py
@@ -6,13 +6,10 @@ from asyncpg.exceptions import UndefinedTableError
 
 from tests.conftest import RESOURCE_ID
 from udata_hydra.cli import (
-    insert_resource_into_catalog,
-    load_catalog,
     purge_checks,
     purge_csv_tables,
     purge_selected_csv_tables,
 )
-from udata_hydra.db.resource import Resource
 
 pytestmark = pytest.mark.asyncio
 nest_asyncio.apply()
@@ -173,58 +170,6 @@ async def test_purge_csv_tables_url_not_in_catalog(setup_catalog, db, fake_check
     assert res is None
     res = await db.fetchrow("SELECT * FROM tables_index WHERE parsing_table = $1", md5)
     assert res is None
-
-
-async def test_load_catalog_url_has_changed(setup_catalog, rmock, db, catalog_content):
-    # the resource url has changed in comparison to load_catalog
-    catalog_content = catalog_content.replace(
-        b"https://example.com/resource-1", b"https://example.com/resource-0"
-    )
-    catalog = "https://example.com/catalog"
-    rmock.get(catalog, status=200, body=catalog_content)
-    await load_catalog(url=catalog, drop_meta=False, drop_all=False, quiet=False)
-    # check that we still only have one entry for this resource in the catalog
-    res = await db.fetch("SELECT * FROM catalog WHERE resource_id = $1", f"{RESOURCE_ID}")
-    assert len(res) == 1
-    assert res[0]["url"] == "https://example.com/resource-0"
-    assert res[0]["deleted"] is False
-
-
-async def test_load_catalog_harvest_modified_at_has_changed(
-    setup_catalog, rmock, db, catalog_content
-):
-    # the harvest_modified_at has changed in comparison to load_catalog
-    catalog_content = catalog_content.replace(b'""\n', b'"2025-03-14 15:49:16.876+02"\n')
-    catalog = "https://example.com/catalog"
-    rmock.get(catalog, status=200, body=catalog_content)
-    await load_catalog(url=catalog, drop_meta=False, drop_all=False, quiet=False)
-
-    # check that harvest metadata has been updated
-    res = await db.fetch("SELECT * FROM catalog WHERE resource_id = $1", f"{RESOURCE_ID}")
-    assert len(res) == 1
-    assert res[0]["harvest_modified_at"] == datetime.fromisoformat(
-        "2025-03-14 15:49:16.876+02"
-    ).replace(tzinfo=timezone.utc)
-
-
-async def test_insert_resource_in_catalog(rmock):
-    new_dataset_id = "a" * 24
-    new_resource_url = "https://new-url.xyz"
-    rmock.get(
-        f"https://www.data.gouv.fr/api/2/datasets/resources/{RESOURCE_ID}/",
-        status=200,
-        payload={
-            "dataset_id": new_dataset_id,
-            "resource": {
-                "id": RESOURCE_ID,
-                "url": new_resource_url,
-            },
-        },
-    )
-    await insert_resource_into_catalog(resource_id=RESOURCE_ID)
-    resource = await Resource.get(RESOURCE_ID)
-    assert resource["dataset_id"] == new_dataset_id
-    assert resource["url"] == new_resource_url
 
 
 @pytest.mark.parametrize(
