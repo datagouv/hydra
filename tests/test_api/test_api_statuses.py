6bbf5b1df75fe83357b6aadad4cc66d3382dfd7a
Adrien Carpentier <me@adriencarpentier.com>
2026-02-04 17:32:37 +0100

    tests: fix tests

--- tests/test_api/test_api_statuses.py
+++ tests/test_api/test_api_statuses.py
@@ -5,26 +5,21 @@ it will interfere with the rest of our async code
 
 import pytest
 
-from udata_hydra.db.resource import Resource
-
 pytestmark = pytest.mark.asyncio
 
 
 async def test_get_crawler_status(setup_catalog, client, fake_check):
-    expected_resources_statuses_count = {s: 0 for s in Resource.STATUSES if s}
-    expected_resources_statuses_count["null"] = 1
     expected_data = {
         "checks": {
-            "pending_count": 1,
-            "fresh_count": 0,
-            "checked_percentage": 0.0,
-            "fresh_percentage": 0.0,
+            "in_progress_count": 0,
+            "in_progress_percentage": 0.0,
+            "needs_check_count": 1,
+            "needs_check_percentage": 100.0,
+            "up_to_date_check_count": 0,
+            "up_to_date_check_percentage": 0.0,
         },
         "resources": {
-            "total_count": 1,
-            "total_filtered_count": 1,
-            "deleted_count": 0,
-            "statuses_count": expected_resources_statuses_count,
+            "total_eligible_count": 1,
         },
     }
 
@@ -33,27 +28,24 @@ async def test_get_crawler_status(setup_catalog, client, fake_check):
     data: dict = await resp.json()
     assert data == expected_data
 
-    expected_resources_statuses_count = {s: 0 for s in Resource.STATUSES if s}
-    expected_resources_statuses_count["null"] = 1
     expected_data = {
         "checks": {
-            "pending_count": 0,
-            "fresh_count": 1,
-            "checked_percentage": 100.0,
-            "fresh_percentage": 100.0,
+            "in_progress_count": 0,
+            "in_progress_percentage": 0.0,
+            "needs_check_count": 0,
+            "needs_check_percentage": 0.0,
+            "up_to_date_check_count": 1,
+            "up_to_date_check_percentage": 100.0,
         },
         "resources": {
-            "total_count": 1,
-            "total_filtered_count": 1,
-            "deleted_count": 0,
-            "statuses_count": expected_resources_statuses_count,
+            "total_eligible_count": 1,
         },
     }
 
     await fake_check()
     resp = await client.get("/api/status/crawler")
     assert resp.status == 200
-    data: dict = await resp.json()
+    data = await resp.json()
     assert data == expected_data
 
 
@@ -76,7 +68,7 @@ async def test_get_stats(setup_catalog, client, fake_check):
     await fake_check(status=500, error="error")
     resp = await client.get("/api/stats")
     assert resp.status == 200
-    data: dict = await resp.json()
+    data = await resp.json()
     assert data == {
         "status": [
             {"label": "error", "count": 1, "percentage": 100.0},
