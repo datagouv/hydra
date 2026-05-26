c24afe5a678bffcd4cb85188cadee2ef8e5aa1f4
Pierlou <pierlou.ramade@data.gouv.fr>
2026-05-26 16:30:53 +0200

    chore: adapt tests

--- tests/test_utils/test_s3_client.py
+++ tests/test_utils/test_s3_client.py
@@ -7,7 +7,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 from pytest_mock import MockerFixture
 
-from udata_hydra.utils.s3 import S3Client
+from udata_hydra.utils.s3 import CONTENT_TYPES, S3Client
 
 
 @pytest.fixture
@@ -27,26 +27,47 @@ def test_s3_client_upload_at_bucket_root(mock_s3: MagicMock, tmp_path: Path) -> 
     client = S3Client(bucket="my-bucket")
     url = client.send_file(f, delete_source=False)
 
-    mock_s3.upload_file.assert_called_once_with(str(f), "file.parquet")
+    mock_s3.upload_file.assert_called_once_with(
+        str(f),
+        "file.parquet",
+        ExtraArgs={
+            "ContentType": CONTENT_TYPES[f.name.split(".")[-1]],
+            "ACL": "public-read",
+        }
+    )
     assert url == "https://s3-example.com/my-bucket/file.parquet"
 
 
 def test_s3_client_upload_with_prefix(mock_s3: MagicMock, tmp_path: Path) -> None:
-    f = tmp_path / "file.parquet"
+    f = tmp_path / "file.geojson"
     f.write_bytes(b"x")
     client = S3Client(bucket="my-bucket", prefix="exports")
     url = client.send_file(f, delete_source=False)
 
-    mock_s3.upload_file.assert_called_once_with(str(f), "exports/file.parquet")
-    assert url == "https://s3-example.com/my-bucket/exports/file.parquet"
+    mock_s3.upload_file.assert_called_once_with(
+        str(f),
+        "exports/file.geojson",
+        ExtraArgs={
+            "ContentType": CONTENT_TYPES[f.name.split(".")[-1]],
+            "ACL": "public-read",
+        }
+    )
+    assert url == "https://s3-example.com/my-bucket/exports/file.geojson"
 
 
 def test_s3_client_upload_changed_pattern(mock_s3: MagicMock, tmp_path: Path, mocker) -> None:
-    f = tmp_path / "file.parquet"
+    f = tmp_path / "file.pmtiles"
     f.write_bytes(b"x")
     mocker.patch("udata_hydra.config.S3_URL_PATTERN", "s3://{bucket}:{endpoint}:{key}")
     client = S3Client(bucket="my-bucket", prefix="exports")
     url = client.send_file(f, delete_source=False)
 
-    mock_s3.upload_file.assert_called_once_with(str(f), "exports/file.parquet")
-    assert url == "s3://my-bucket:s3-example.com:exports/file.parquet"
+    mock_s3.upload_file.assert_called_once_with(
+        str(f),
+        "exports/file.pmtiles",
+        ExtraArgs={
+            "ContentType": CONTENT_TYPES[f.name.split(".")[-1]],
+            "ACL": "public-read",
+        }
+    )
+    assert url == "s3://my-bucket:s3-example.com:exports/file.pmtiles"
