902660297427515bf7e5019004b7fe19b327d538
Pierlou <pierlou.ramade@data.gouv.fr>
2026-06-16 09:32:45 +0200

    refactor: fallback to os tmp folder in true_path

--- udata_hydra/utils/file.py
+++ udata_hydra/utils/file.py
@@ -131,4 +131,4 @@ def remove_remainders(resource_id: str, extensions: list[str]) -> None:
 def true_path(file_name: str):
     if file_name.startswith("tests/data/"):
         return file_name
-    return f"{config.TEMPORARY_DOWNLOAD_FOLDER or '/tmp'}/{file_name}"
+    return os.path.join(config.TEMPORARY_DOWNLOAD_FOLDER or tempfile.gettempdir(), file_name)
