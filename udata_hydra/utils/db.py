f71a3d9022116ff072edbe501873198546c143d5
Adrien Carpentier <me@adriencarpentier.com>
2026-06-23 18:38:53 +0200

    refactor: move private test helper _ get_columns_with_indexes

--- udata_hydra/utils/db.py
+++ udata_hydra/utils/db.py
@@ -53,37 +53,3 @@ async def update_table_record(table_name: str, record_id: int, data: dict) -> Re
     q = compute_update_query(table_name, data)
     pool = await context.pool()
     return await pool.fetchrow(q, *data.values(), record_id)
-
-
-async def get_columns_with_indexes(table_name: str) -> list[Record]:
-    """
-    Get the columns of a table which have indexes
-    Return a list of records with the following columns:
-        - table_name
-        - index_name
-        - column_name
-    """
-    pool = await context.pool()
-    async with pool.acquire() as connection:
-        q = """
-            SELECT
-                table_class.relname AS table_name,
-                index_class.relname AS index_name,
-                attribute.attname AS column_name
-            FROM
-                pg_class table_class,
-                pg_class index_class,
-                pg_index index_relation,
-                pg_attribute attribute
-            WHERE
-                table_class.oid = index_relation.indrelid
-                AND index_class.oid = index_relation.indexrelid
-                AND attribute.attrelid = table_class.oid
-                AND attribute.attnum = ANY(index_relation.indkey)
-                AND table_class.relkind = 'r'
-                AND table_class.relname = $1
-            ORDER BY
-                table_class.relname,
-                index_class.relname;
-        """
-        return await connection.fetch(q, table_name)
