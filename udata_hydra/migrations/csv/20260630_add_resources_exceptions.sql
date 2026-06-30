456a3721149b3cb3bb6c6cd07cb6507726c408de
Alexandre Bulté <alexandre@bulte.net>
2026-06-30 17:05:20 +0200

    Apply suggestion from @abulte

--- udata_hydra/migrations/csv/20260630_add_resources_exceptions.sql
+++ udata_hydra/migrations/csv/20260630_add_resources_exceptions.sql
@@ -1,6 +1,6 @@
 -- creates the table from `main/20240827_add_resources_exceptions_table.sql`
 -- which migration has been documented in `csv/20250610_migrate_resources_exception.sql` but is a noop
--- this helps having a working env from scratch but does not apply on prod (IF NOT EXIST)
+-- this helps having a working env from scratch but does not apply on prod (IF NOT EXISTS)
 
 CREATE TABLE IF NOT EXISTS resources_exceptions (
     id SERIAL PRIMARY KEY,
